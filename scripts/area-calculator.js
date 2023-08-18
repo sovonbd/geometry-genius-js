2f47c675f8ca882e8a40e70ce3ae91dffbe69f2c
sovonbd <sovonsdp@gmail.com>
2023-08-17 19:07:40 -0500

    updated

--- scripts/area-calculator.js
+++ scripts/area-calculator.js
@@ -24,3 +24,33 @@ function calculateRectangleArea() {
   const rectangleAreaSpan = document.getElementById("rectangle-area");
   rectangleAreaSpan.innerText = area;
 }
+
+function calculateParallelogramArea() {
+  const base = getInputValue("parallelogram-base");
+  const height = getInputValue("parallelogram-height");
+
+  const area = base * height;
+  setElementInnerText("parallelogram-area", area);
+}
+
+function calculateEllipseArea() {
+  const majorRadius = getInputValue("major-radius");
+  const minorRadius = getInputValue("minor-radius");
+
+  const area = (Math.PI * majorRadius * minorRadius).toFixed(2);
+  setElementInnerText("ellipse-area", area);
+}
+
+// reusable get input
+function getInputValue(fieldId) {
+  const inputField = document.getElementById(fieldId);
+  const inputText = inputField.value;
+  const input = parseFloat(inputText);
+  return input;
+}
+
+// reusable set
+function setElementInnerText(elementID, area) {
+  const element = document.getElementById(elementID);
+  element.innerText = area;
+}
